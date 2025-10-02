352c359d035ca89097be536facfa105172a114a7
petitcoke <petitscoke@gmail.com>
2025-10-02 19:56:07 +0200

    Added Scripts: InsightsFragment.kt fragment_insights.xml
    Added Drawables:
    divider_vertical.xml
    ic_bottom_nav_colors.xml
    ic_check_circle.xml
    ic_insights.xml
    ic_lightbulb.xml
    
    Edited:
    MainActivity.kt
    colors.xml
    bottom_navigation_menu.xml

--- app/src/main/java/za/ac/iie/TallyUp/MainActivity.kt
+++ app/src/main/java/za/ac/iie/TallyUp/MainActivity.kt
@@ -9,6 +9,7 @@ import za.ac.iie.TallyUp.ui.TransactionsFragment
 import za.ac.iie.TallyUp.ui.budget.BudgetDashboardFragment
 import za.ac.iie.TallyUp.ui.GoalsFragment
 import za.ac.iie.TallyUp.ui.ProfileFragment
+import za.ac.iie.TallyUp.ui.insights.InsightsFragment // Add this import
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupToolbar()
-        setupBottomNavigation()
+        setupBottomNavigation() // This calls the single function
         setupFAB()
 
         // Load default fragment
@@ -51,6 +52,10 @@ class MainActivity : AppCompatActivity() {
                 }
                 R.id.navigation_goals -> {
                     loadFragment(GoalsFragment())
+                    true
+                }
+                R.id.navigation_insights -> { // Add insights navigation
+                    loadFragment(InsightsFragment())
                     true
                 }
                 R.id.navigation_profile -> {
